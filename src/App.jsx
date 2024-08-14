d727919754e116d792ccf345acd880bbd689795e
Umut-Polat-GM <umut.polat.272@gmail.com>
2024-08-14 19:44:03 +0300

    Update Login import in App.jsx and update npm dependencies
    feat: Update Login import in App.jsx
    chore: Update npm dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { Box } from "@mui/material";
-import Login from "./pages/Login";
 import Notifications from "./components/Notifications";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./auth/Login";
 
 const App = () => {
-  return (
-    <Box>
-      <Notifications />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-      </Routes>
-    </Box>
-  );
+    return (
+        <Box>
+            <Notifications />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="login" element={<Login />} />
+            </Routes>
+        </Box>
+    );
 };
 
 export default App;
